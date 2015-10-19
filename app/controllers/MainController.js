8f918f338710a31dcc3be7a721dcf56f854e6ccb
Peter Johnson <insomnia@rcpt.at>
2015-10-19 16:55:20 +0200

    bump currentVersion

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -121,7 +121,7 @@ function MainController( $scope, $location, $http, $rootScope ){
   };
   console.info( 'funfact: you can use getEndpoints() and setEndpoints() to change which hosts are being queried, or use resetEndpoints() to reset to defaults');
 
-  var currentVersion = 1;
+  var currentVersion = 2;
   var version = window.localStorage.getItem('version');
   if( isNaN(version) || parseInt( version, 10 ) < currentVersion ){
     window.resetEndpoints();
